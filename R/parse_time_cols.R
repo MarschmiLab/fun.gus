677ac66da9218ee3c3253dbb3b78abada5e02f4c
gus=pendleton <guspendleton@gmail.com>
2023-09-01 09:51:00 -0400

    Init package

---
+++ R/parse_time_cols.R
@@ -0,0 +1,29 @@
+library(dplyr)
+library(tidyr)
+library(lubridate)
+#' Parse the terrible export of date times from our Data Log
+#'
+#' You have probably noticed the Google Data log backups have very weird export formats. This function helps parse them to more usable times. This function is just extracted the time from a poorly formatted time column; if you want the full date time, you'll need to parse and combine a date column after using this function to get a clean time column.
+#' @param dataframe Your dataframe (or tibble)
+#' @param bad_column The poorly formatted time column, not in quotes. Values of this column usually start with "Sat Dec 30".
+#' @param new_name The name for your new, well-formatted column, not in quotes.
+#' @returns A dataframe with the bad column removed and a fresh column inserted.
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+#' @importFrom tidyr separate
+#' @importFrom lubridate as_datetime
+#' @importFrom magrittr %>%
+#' @export
+parse_time_cols <- function(dataframe, bad_column, new_name){
+  bc = enquo(bad_column)
+  nn = enquo(new_name)
+
+  dataframe %>%
+    tidyr::separate(!!bc,  sep = " ", extra = "drop",into = c("Weekday", "Month", "Day", "Year", "Time", "Tmz"))%>%
+    dplyr::mutate(!!nn := lubridate::as_datetime(Time, format = "%H:%M:%S"))%>%
+    dplyr::select(-Weekday, -Month, -Day, -Year, -Time,  - Tmz)
+}
+
+df%>%
+  parse_times(Start_Filtration_Time, Good_Start)%>%
+  parse_times(End_Filtration_Time, Good_End)
